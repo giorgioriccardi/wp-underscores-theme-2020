45e73274018af87d7aa3d815c16124c6190489f6
Giorgio Riccardi <giorgio.riccardi.ca@gmail.com>
2020-10-16 15:48:56 -0700

    initial functions setup

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Latitude51 functions and definitions
  *
@@ -7,12 +8,12 @@
  * @package Latitude51
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+if (!defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define('_S_VERSION', '1.0.0');
 }
 
-if ( ! function_exists( 'latitude51_setup' ) ) :
+if (!function_exists('latitude51_setup')) :
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
@@ -20,17 +21,18 @@ if ( ! function_exists( 'latitude51_setup' ) ) :
 	 * runs before the init hook. The init hook is too late for some features, such
 	 * as indicating support for post thumbnails.
 	 */
-	function latitude51_setup() {
+	function latitude51_setup()
+	{
 		/*
 		 * Make theme available for translation.
 		 * Translations can be filed in the /languages/ directory.
 		 * If you're building a theme based on Latitude51, use a find and replace
 		 * to change 'latitude51' to the name of your theme in all the template files.
 		 */
-		load_theme_textdomain( 'latitude51', get_template_directory() . '/languages' );
+		load_theme_textdomain('latitude51', get_template_directory() . '/languages');
 
 		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
+		add_theme_support('automatic-feed-links');
 
 		/*
 		 * Let WordPress manage the document title.
@@ -38,19 +40,49 @@ if ( ! function_exists( 'latitude51_setup' ) ) :
 		 * hard-coded <title> tag in the document head, and expect WordPress to
 		 * provide it for us.
 		 */
-		add_theme_support( 'title-tag' );
+		add_theme_support('title-tag');
 
 		/*
 		 * Enable support for Post Thumbnails on posts and pages.
 		 *
 		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		 */
-		add_theme_support( 'post-thumbnails' );
+		add_theme_support('post-thumbnails');
+
+		// ssws2020
+		// Set post thumbnail size.
+		set_post_thumbnail_size(1200, 9999);
+
+		// Add custom image size used in Cover Template.
+		add_image_size('latitude51-fullscreen', 1980, 9999);
+
+		// Custom logo.
+		$logo_width  = 120;
+		$logo_height = 90;
+
+		// If the retina setting is active, double the recommended width and height.
+		if (get_theme_mod('retina_logo', false)) {
+			$logo_width  = floor($logo_width * 2);
+			$logo_height = floor($logo_height * 2);
+		}
+
+		// Add support for full and wide align images.
+		add_theme_support('align-wide');
+
+		add_theme_support(
+			'custom-logo',
+			array(
+				'height'      => $logo_height,
+				'width'       => $logo_width,
+				'flex-height' => true,
+				'flex-width'  => true,
+			)
+		);
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus(
 			array(
-				'menu-1' => esc_html__( 'Primary', 'latitude51' ),
+				'menu-1' => esc_html__('Primary', 'latitude51'),
 			)
 		);
 
@@ -84,7 +116,7 @@ if ( ! function_exists( 'latitude51_setup' ) ) :
 		);
 
 		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
+		add_theme_support('customize-selective-refresh-widgets');
 
 		/**
 		 * Add support for core custom logo.
@@ -102,7 +134,7 @@ if ( ! function_exists( 'latitude51_setup' ) ) :
 		);
 	}
 endif;
-add_action( 'after_setup_theme', 'latitude51_setup' );
+add_action('after_setup_theme', 'latitude51_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -111,22 +143,24 @@ add_action( 'after_setup_theme', 'latitude51_setup' );
  *
  * @global int $content_width
  */
-function latitude51_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'latitude51_content_width', 640 );
+function latitude51_content_width()
+{
+	$GLOBALS['content_width'] = apply_filters('latitude51_content_width', 640);
 }
-add_action( 'after_setup_theme', 'latitude51_content_width', 0 );
+add_action('after_setup_theme', 'latitude51_content_width', 0);
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function latitude51_widgets_init() {
+function latitude51_widgets_init()
+{
 	register_sidebar(
 		array(
-			'name'          => esc_html__( 'Sidebar', 'latitude51' ),
+			'name'          => esc_html__('Sidebar', 'latitude51'),
 			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'latitude51' ),
+			'description'   => esc_html__('Add widgets here.', 'latitude51'),
 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</section>',
 			'before_title'  => '<h2 class="widget-title">',
@@ -134,22 +168,23 @@ function latitude51_widgets_init() {
 		)
 	);
 }
-add_action( 'widgets_init', 'latitude51_widgets_init' );
+add_action('widgets_init', 'latitude51_widgets_init');
 
 /**
  * Enqueue scripts and styles.
  */
-function latitude51_scripts() {
-	wp_enqueue_style( 'latitude51-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'latitude51-style', 'rtl', 'replace' );
+function latitude51_scripts()
+{
+	wp_enqueue_style('latitude51-style', get_stylesheet_uri(), array(), _S_VERSION);
+	wp_style_add_data('latitude51-style', 'rtl', 'replace');
 
-	wp_enqueue_script( 'latitude51-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script('latitude51-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
+	if (is_singular() && comments_open() && get_option('thread_comments')) {
+		wp_enqueue_script('comment-reply');
 	}
 }
-add_action( 'wp_enqueue_scripts', 'latitude51_scripts' );
+add_action('wp_enqueue_scripts', 'latitude51_scripts');
 
 /**
  * Implement the Custom Header feature.
@@ -174,7 +209,6 @@ require get_template_directory() . '/inc/customizer.php';
 /**
  * Load Jetpack compatibility file.
  */
-if ( defined( 'JETPACK__VERSION' ) ) {
+if (defined('JETPACK__VERSION')) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
-
