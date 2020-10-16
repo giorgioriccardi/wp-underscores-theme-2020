2ea6d11464fd53d8546f35b6412f5aeba860ef8b
Giorgio Riccardi <giorgio.riccardi.ca@gmail.com>
2020-10-16 09:26:49 -0700

    replace screenshot with latitude51

---
+++ inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Latitude51 Theme Customizer
+ *
+ * @package Latitude51
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function latitude51_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'latitude51_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'latitude51_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'latitude51_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function latitude51_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function latitude51_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function latitude51_customize_preview_js() {
+	wp_enqueue_script( 'latitude51-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'latitude51_customize_preview_js' );
